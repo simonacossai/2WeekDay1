800bfddbbda945e1f8c4b33d2400456fd3bbc307
simonacossai <cossaisimona@gmail.com>
2020-10-05 20:55:33 +0200

    DONE- extra function for manipulate DOM

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 let info={};
 function sendData() {
-    let firstInput = document.querySelector('#name')
-    let secondInput = document.querySelector('#email')
-    let messageInput = document.querySelector('#message')
+    let firstInput = document.querySelector('#name');
+    let secondInput = document.querySelector('#email');
+    let messageInput = document.querySelector('#message');
 
 
     info["name"]= firstInput.value;
     info["email"]= secondInput.value;
     info["message"]= messageInput.value;
 
-    /*console.log('Your name is', firstInput.value)
-    console.log('Your surname is', surnameInput.value)
-    console.log('Your email is', emailInput.value)*/
-
     console.log(info);
-    firstInput.value = ''
-    secondInput.value = ''
-    messageInput.value = ''
+    firstInput.value = '';
+    secondInput.value = '';
+    messageInput.value = '';
+  }
+
+
+  function changeHeadingText(){
+    let heading = document.querySelector('.brandName');
+    heading.style.color = "red";
+  }
+
+  function changeBodyColor(){
+    let page = document.querySelector('body');
+    page.style.backgroundColor= "yellow";
+    
+  }
+
+  function changeAddress(){
+    let address = document.querySelector('#address');
+    address.innerHTML = "Via Roma, 72";
   }
